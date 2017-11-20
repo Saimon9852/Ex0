bbf662a4aa5639360d5579f00af7348d81f2e799
Rachel Plaksin <Rachel Plaksin@LAPTOP-7G21PRTE>
2017-11-20 22:39:46 +0200

    added tests

---
+++ projectEx0/src/mypack/CsvTest.java
@@ -0,0 +1,35 @@
+package mypack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CsvTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCsv() {
+		
+		
+	}
+
+	@Test
+	public void testSetCsv() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFiles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWritescan() {
+		fail("Not yet implemented");
+	}
+
+}
