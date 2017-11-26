76609cbe0c3c9f86ee38b46bb30df74962ee23de
Rachel Plaksin <Rachel Plaksin@LAPTOP-7G21PRTE>
2017-11-26 12:23:47 +0200

    upgraded tests

--- projectEx0/src/mypack/TokmlTest.java
+++ projectEx0/src/mypack/TokmlTest.java
@@ -2,6 +2,7 @@ package mypack;
 
 import static org.junit.Assert.*;
 
+<<<<<<< Upstream, based on branch 'xGreenbean-patch-1' of https://github.com/xGreenbean/Ex0.git
 import java.util.ArrayList;
 
 import org.junit.Before;
@@ -148,5 +149,104 @@ public  class TokmlTest {
 	public void testHowManyexist() {
 		fail("Not yet implemented");
 	}
+=======
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TokmlTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTokml() throws DataException {
+		System.out.println("***test***"+"\n"+"Test of-Tokml Builder"+"\n"
+				);
+		Csv Ariel=new Csv("C:\\Users\\Rachel Plaksin\\Desktop\\Ariel");
+		Ariel.writescan("take2");
+		Tokml kml=new Tokml("take2.csv");
+		//first we will check that there are less then 10 points on each scan.
+		for(int i=0;i<kml.getDB().size();i++){
+			if(kml.getDB().get(i).getSpots().size()>10){
+				fail("more then 10 points in the scan");
+			}
+		}
+		//now we will check that the macim are currect.
+		//we will do so by checking one of the macs in macim
+		//and seeing if it is the strongest one in the DB
+		Random rand = new Random();
+		int  n = rand.nextInt(kml.getMacim().size());
+		for(int i=0;i<kml.getDB().size();i++){
+			for(int j=0;j<kml.getDB().get(i).getSpots().size();j++){
+				if(kml.getMacim().get(n).getMac().equals(kml.getDB().get(i).getSpots().get(j).getMac())){
+					if(Integer.parseInt(kml.getMacim().get(n).getRssi())
+							<Integer.parseInt(kml.getDB().get(i).getSpots().get(j).getRssi())){
+						fail("the mac in macim isnt the strogenst one");
+					}
+				}
+			}
+		}
+	}
+
+	@Test
+	public void testGetDB() {
+		Tokml kml=new Tokml();
+		if((kml.getDB()!=null)){
+		}else{
+			fail("empty builder failed");
+		}
+	}
+
+	@Test (expected = DataException.class)
+	// here we test the create by filter
+	//we will give filter data that will not result with any matching
+	// we will then get an exception.
+	public void testCreateKmlByFilter() throws DataException {
+		System.out.println("***test***"+"\n"+"Test of-CreatekmlByFilter"+"\n"+
+	"***user input required***"+"\n");
+		Csv Ariel=new Csv("C:\\Users\\Rachel Plaksin\\Desktop\\Ariel");
+		Ariel.writescan("take2");
+		Tokml kml=new Tokml("take2.csv");
+		kml.CreateKmlByFilter("test");
+	}
+
+	@Test
+	//because we have user input here,
+	//the tester must press 1 inorder for this test to work.
+	public void testUserInput1() {
+		System.out.println("***test***"+"\n"+"Test of-UserInput"+"\n"+
+				"***user input required***"+"\n");
+		Tokml kml = new Tokml();
+		String input=kml.userInput();
+		  assertEquals(input,"Date");
+	
+	}
+	@Test
+	//because we have user input here,
+	//the tester must press 2 inorder for this test to work.
+	public void testUserInput2() {
+		System.out.println("***test***"+"\n"+"Test of-UserInput"+"\n"+
+				"***user input required***"+"\n");
+		Tokml kml = new Tokml();
+		String input=kml.userInput();
+		  assertEquals(input,"Location");
+	
+	}
+	@Test
+	//because we have user input here,
+	//the tester must press any number but "1" and "2" inorder for this test to work.
+	public void testUserInput3() {
+		System.out.println("***test***"+"\n"+"Test of-UserInput"+"\n"+
+				"***user input required***"+"\n");
+		Tokml kml = new Tokml();
+		String input=kml.userInput();
+		  assertEquals(input,"None");
+	
+	}
+	
+>>>>>>> 11210b0 upgraded tests
 
 }
